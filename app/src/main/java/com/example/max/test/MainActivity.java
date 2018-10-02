c6bb596b28ccbcff4a7ce0fd4dc18955b4970b8a
H4dDock <maxolo.di@gmail.com>
2018-10-03 01:44:50 +0300

    Git test1. Project v0.00

--- app/src/main/java/com/example/max/test/MainActivity.java
+++ app/src/main/java/com/example/max/test/MainActivity.java
@@ -10,25 +10,12 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imagetView;
-    Button button;
-    News newNews;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ArrayList<News> listNews = new ArrayList<>();
-        listNews.add(new News("test title", "test info"));
-        listNews.add(new News("test title", "test info"));
-
-        imagetView = findViewById(R.id.imageView);
-        button = findViewById(R.id.button);
     }
 
     public void HobaListener(View view) {
-        button.setVisibility(View.INVISIBLE);
-        imagetView.setVisibility(View.VISIBLE);
     }
 }
